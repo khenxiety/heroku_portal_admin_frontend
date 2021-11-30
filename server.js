43f1588ca1d446ff30e2ac01adf0650aa5745f77
khenxiety <74524968+khenxiety@users.noreply.github.com>
2021-11-30 09:44:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 // serve only the static files  form the dist directory
 app.use(express.static('/dist/frontend'));
 
-app.get('/*',(req,res)=>{
-    res.sendFile('index.html',{root:'dist/angular-heroku/'});
-})
+app.get('*', (req, res) => {
+    res.sendFile(`./frontend/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 app.listen(process.env.PORT || 8080);
