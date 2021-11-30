a88f535c728ec7f686f00d2964ea486a8894177e
khenxiety <74524968+khenxiety@users.noreply.github.com>
2021-11-30 09:48:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-//install express server
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// serve only the static files  form the dist directory
-app.use(express.static('/dist/frontend'));
-
-app.get('*', (req, res) => {
-    res.sendFile(`./frontend/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
-
+app.use(express.static(__dirname + '/dist/frontend'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/frontend/index.html'));});
 app.listen(process.env.PORT || 8080);
